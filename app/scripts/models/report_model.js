fa52f59dce901dae94ebb3cc423627839cb5b9b6
Nathan Smith <nathan@hebemedia.com>
2015-02-03 17:43:52 +0000

    Hooked finance pages up to ckan data

--- app/scripts/models/report_model.js
+++ app/scripts/models/report_model.js
@@ -2,10 +2,12 @@ Dashboard.IndicatorModel = Ember.Object.extend({
     targetValue: null,
     currentValue: null,
     previousValue: null,
+    _dataValues: null,
+
     chartID: function () {
         return 'Chart' + this.get('ID');
     }.property(),
-    _dataValues: null,
+
     dataValues: function () {
         if (this.get('_dataValues') != null) {
             return this.get('_dataValues');
@@ -21,9 +23,6 @@ Dashboard.IndicatorModel = Ember.Object.extend({
                     currentValue: results[0],
                     previousValue: results[1]
                 });
-                //obj.set('_dataValues', results);
-                //obj.set('currentValue', results[0]);
-                //obj.set('previousValue', results[1]);
                 return obj.get('_dataValues');
             });
         }
@@ -34,9 +33,49 @@ Dashboard.IndicatorModel.reopenClass({
     values: null
 });
 
-Dashboard.ReportModel = Ember.Object.extend({});
-Dashboard.ReportModel.reopenClass({
 
+
+
+Dashboard.PageModel = Ember.Object.extend({
+    _rows: null,
+    rows: function () {
+        switch (this.get('Type')) {
+            case "page_finance":
+            case "page_finance_table":
+                if (this.get('_rows') !== null) {
+                    return this.get('_rows');
+                } else {
+                    if (this.get('CKANID') !== null) {
+                        var obj = this;
+                        var ckanID = obj.get('CKANID');
+                        $.ajax({
+                            url: 'http://54.154.11.196/api/action/datastore_search_sql?sql=SELECT * from "' + ckanID + '"'
+                        })
+                        .then(function (response) {
+                            var results = response.result.records;
+                            obj.setProperties({
+                                _rows: results
+                            });
+                            return obj.get('_rows');
+                        });
+                    }
+                }
+                break;
+        }
+        return this.get('_rows');
+    }.property()
+});
+
+
+
+
+
+
+
+Dashboard.ReportModel = Ember.Object.extend({
+});
+
+Dashboard.ReportModel.reopenClass({
     findAll: function (resource_id) {
         var data = {
             resource_id: 'f441a675-1d3c-43d8-aa8d-c1059381c0e8', // the resource id
@@ -52,10 +91,11 @@ Dashboard.ReportModel.reopenClass({
             var types = {};
 
             result.records.forEach(function (item) {
-                if (!types[item.Type]) {
-                    types[item.Type] = [];
+                var itemTypeGeneric = item.Type.split('_')[0]; // e.g. page_finance will be added as 'page' but still contain its true type in its own Type property
+                if (!types[itemTypeGeneric]) {
+                    types[itemTypeGeneric] = [];
                 }
-                types[item.Type].push(item);
+                types[itemTypeGeneric].push(item);
             });
 
             types['page'].forEach(function (page) {
@@ -69,11 +109,17 @@ Dashboard.ReportModel.reopenClass({
             });
 
             types['annex'].forEach(function (annex) {
-                annex.pages = types['page'].filterBy('ParentID', annex.ID);
+                var pageTypes = types['page'].filterBy('ParentID', annex.ID);
+                var pages = [];
+                pageTypes.forEach(function (item) {
+                    var page = Dashboard.PageModel.create(item);
+                    pages.push(page);
+                });
+                annex.pages = pages;
             });
 
             var report = Dashboard.ReportModel.create({
-                title: 'Hi Im a report',
+                title: 'Sample Board Report',
                 annexes: types['annex']
             });
 
@@ -91,5 +137,4 @@ Dashboard.ReportModel.reopenClass({
         //  }
         //);
     }
-
 });
