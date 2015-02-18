86af477dd30f1f8e568f466c05cebdeb611562c0
Nathan Smith <nathan@hebemedia.com>
2015-02-18 23:43:45 +0000

    Supplementary Business Area - pulled from new CKAN Resource

--- app/scripts/models/page_model.js
+++ app/scripts/models/page_model.js
@@ -4,31 +4,34 @@
 Dashboard.PageModel = Ember.Object.extend({
     _rows: null,
     rows: function () {
-        switch (this.get('Type')) {
-            case "page_finance":
-            case "page_finance_table":
-                if (this.get('_rows') !== null) {
-                    return this.get('_rows');
-                } else {
-                    if (this.get('CKANID') !== null) {
-                        var obj = this;
-                        var ckanID = obj.get('CKANID');
-                        $.ajax({
-                            url: 'http://54.154.11.196/api/action/datastore_search_sql?sql=SELECT * from "' + ckanID + '"'
-                        })
-                        .then(function (response) {
-                            var results = response.result.records;
-                            obj.setProperties({
-                                _rows: results
+        if (this.get('_rows') === null) {
+            switch (this.get('Type')) {
+                case "page_finance":
+                case "page_finance_table":
+                case "page_supplimentary_business":
+                    if (this.get('_rows') !== null) {
+                        return this.get('_rows');
+                    } else {
+                        if (this.get('CKANID') !== null) {
+                            var obj = this;
+                            var ckanID = obj.get('CKANID');
+                            $.ajax({
+                                url: 'http://54.154.11.196/api/action/datastore_search_sql?sql=SELECT * from "' + ckanID + '"'
+                            })
+                            .then(function (response) {
+                                var results = response.result.records;
+                                obj.setProperties({
+                                    _rows: results
+                                });
+                                return obj.get('_rows');
                             });
-                            return obj.get('_rows');
-                        });
+                        }
                     }
-                }
-                break;
+                    break;
+            }
         }
         return this.get('_rows');
-    }.property(),
+    }.property('_rows'),
 
     _ragStatus: null,
     ragStatus: function () {
@@ -39,7 +42,7 @@ Dashboard.PageModel = Ember.Object.extend({
             var status = [
                 {
                     colour: 'red',
-                    count: utils.random(1,4),
+                    count: utils.random(1, 4),
                     previous: utils.random(-6, -1)
                 },
                 {
