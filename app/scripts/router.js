c45d3d6e9aa89c6e4faebb61cc173c73c1e56812
Nathan Smith <nathan@hebemedia.com>
2015-03-12 15:25:29 +0000

    Changed some page-to-page linking to be more generic when deployed at different rootUrls

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -5,3 +5,18 @@ Dashboard.Router.map(function () {
         });
     });
 });
+
+Dashboard.Router.reopen({
+    rootURL: '/',
+    init: function () {
+        // set rootURL using regex to extract appropriate
+        // rootURL based on current window location
+        var matchedURL = window.location.pathname.match('[^/]*/[^/]*');
+        console.log('matched url = ' + matchedURL);
+        if (matchedURL !== null && matchedURL.length > 0) {
+            console.log('setting rootUrl to ' + matchedURL[0]);
+            this.set('rootURL', matchedURL[0]);
+        }
+        this._super();
+    }
+});
